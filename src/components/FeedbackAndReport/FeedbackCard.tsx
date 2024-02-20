e43f0d7d6c8defb14047fc6e021c24bd14ee8896
ananthu2205 <svananthu2@gmail.com>
2024-02-20 20:24:57 +0530

    Fix card

--- src/components/FeedbackAndReport/FeedbackCard.tsx
+++ src/components/FeedbackAndReport/FeedbackCard.tsx
@@ -15,6 +15,7 @@ interface FeedbackCardProps {
 }
 
 const FeedbackCard: FC<FeedbackCardProps | any> = forwardRef(({ sectionData, index, expandIndex, setExpandIndex, user }, ref) => {
+    const [isturncate, setIsturncate] = useState(true);
     const childRef: any = useRef(null);
     const [isExpanded, setIsExpanded] = useState(false);
     const [promptSelect, setPromptSelect] = useState('');
@@ -78,7 +79,7 @@ const FeedbackCard: FC<FeedbackCardProps | any> = forwardRef(({ sectionData, ind
         trigger: () => {
             setIsExpanded(true);
             setIsDescriptionExpanded(true)
-            console.log(index)
+            setIsturncate(false)
         }
     }));
     const handleOnCardClick = () => {
@@ -90,6 +91,7 @@ const FeedbackCard: FC<FeedbackCardProps | any> = forwardRef(({ sectionData, ind
         }
 
         setIsExpanded(false);
+        setIsturncate(true)
 
     }
     const openCardExpansion = (e: any) => {
@@ -97,120 +99,124 @@ const FeedbackCard: FC<FeedbackCardProps | any> = forwardRef(({ sectionData, ind
         e.stopPropagation();
         console.log('trigger');
         isDescriptionExpanded ? setIsExpanded(!isExpanded) : setIsDescriptionExpanded(!isDescriptionExpanded)
+        setIsturncate(false)
 
     }
     console.log("sectionDatasectionData", preQuestionClarity);
     return (
         <>
-        
-                <Paper elevation={0} sx={{ margin: 2, padding: 2 }} className="card" ref={cardRef} onClick={handleOnCardClick}>
-                    <div className={`innerContainer ${(index == expandIndex) && isDescriptionExpanded ? "applyBorder" : "removeBorder"}`} onMouseEnter={() => { cardRef.current.classList.remove("removeBorder"); cardRef.current.classList.add("applyBorder") }} onMouseLeave={() => { cardRef.current.classList.remove("applyBorder"); cardRef.current.classList.add("removeBorder"); }}>
-                        {
-                            isExpanded ?
-                                <Typography variant="h6" className="details">Step {index + 1} </Typography> :
-                                <Typography variant="h6" className="details">Step {index + 1} out of {totalSections}  </Typography>
-                        }
 
-                        <div className='arrowbutton' >
-                            {isExpanded ?
-                                <Button className='expandbutton' onClick={() => { setIsExpanded(!isExpanded); setIsDescriptionExpanded(!isDescriptionExpanded) }}><ExpandMoreOutlined /></Button> :
-                                <Button className={(index == expandIndex) && isDescriptionExpanded ? "arrowbuttonActive" : "buttonArrow"} onClick={(e) => openCardExpansion(e)}><ArrowForward /></Button>}
-                        </div>
-                        <Typography variant="subtitle1" className={`${(index == expandIndex) && isDescriptionExpanded ? "titleTextAdd" : "titleTextupdated"}`}>{currentSection['name']}</Typography>
-                        {(index == expandIndex) && isDescriptionExpanded &&
-                            <div className="viewmore">
-                                <div className='mt-2 f-14 fw-400 pr-3 formateText addmore'>{currentSection['description']}</div>
-                            </div>
-                        }
-                        <Box style={{marginTop: "15px"}}>
-                        <img
-                            src="/images/icons/Expandbutton.svg"
-                            alt="guidance"></img>
-                        </Box>
-                        {!isExpanded &&
-                            <div style={{ display: 'flex', marginTop: "48px" }}>
-                                <Typography className='cardFooterText'>{currentSection['promptQuestionsMap'].length} Prompts</Typography>
-                                <Divider orientation="vertical" flexItem style={{ height: '20px', margin: '0 10px' }} />
-                                <Typography className='cardFooterText'>{currentSection['durationInMins']} mins</Typography>
-                            </div>
-                        }
+            <Paper elevation={0} sx={{ margin: 2, padding: 2 }} className={`card ${(index == expandIndex) && isDescriptionExpanded ? "applyBorder" : isExpanded ? "removeBorder" : ""}`} ref={cardRef} onClick={handleOnCardClick} onMouseEnter={() => { if (!isExpanded) { cardRef.current.classList.remove("applyBorder"); } cardRef.current.classList.add("applyBorder") }} onMouseLeave={() => { if (!isExpanded) { cardRef.current.classList.remove("applyBorder"); } }}>
+                <div className={`innerContainer `} >
+                    {
+                        isExpanded ?
+                            <Typography variant="h6" className="details">Step {index + 1} </Typography> :
+                            <Typography variant="h6" className="details">Step {index + 1} out of {totalSections}  </Typography>
+                    }
+
+                    <div className='arrowbutton' >
+                        {isExpanded ?
+                            <Button className='expandbutton' onClick={() => { setIsExpanded(!isExpanded); setIsDescriptionExpanded(!isDescriptionExpanded) }}><ExpandMoreOutlined /></Button> :
+                            <Button className={(index == expandIndex) && isDescriptionExpanded ? "arrowbuttonActive" : "buttonArrow"} onClick={(e) => openCardExpansion(e)}><ArrowForward /></Button>}
                     </div>
+                    <Typography variant="subtitle1" className={`${(index == expandIndex) && isDescriptionExpanded && isExpanded ? "titleTextAdd" : "titleTextupdated"}`}>{currentSection['name']}</Typography>
+                    {(index == expandIndex) && isDescriptionExpanded &&
+                        <div className="viewmore">
+                            <div className={`mt-2 f-14 fw-400 pr-3 formateText ${isturncate ? 'addmore' : ''}`}>{currentSection['description']}</div>
+                        </div>
+                    }
+                    {isDescriptionExpanded &&
+                        <Box style={{ marginTop: "15px" }} onClick={(e) => { e.stopPropagation(); setIsturncate(!isturncate) }}>
+                            <img
+                                style={{ transform: `rotate(${isturncate ? '0' : '180deg'})` }}
+                                src="/images/icons/Expandbutton.svg"
+                                alt="guidance"></img>
+                        </Box>
+                    }
+                    {!isExpanded &&
+                        <div style={{ display: 'flex', marginTop: "48px" }}>
+                            <Typography className='cardFooterText'>{currentSection['promptQuestionsMap'].length} Prompts</Typography>
+                            <Divider orientation="vertical" flexItem style={{ height: '20px', margin: '0 10px' }} />
+                            <Typography className='cardFooterText'>{currentSection['durationInMins']} mins</Typography>
+                        </div>
+                    }
+                </div>
 
-                    {(index == expandIndex) && isDescriptionExpanded && isExpanded &&
-                        <>
+                {(index == expandIndex) && isDescriptionExpanded && isExpanded &&
+                    <>
 
-                            <div className='addBorderContainer'>
-                                {expandPrompt == -1 ? <>
-                                    <div className='sectionClarify' style={{marginTop: "20px"}}>
+                        <div className='addBorderContainer'>
+                            {expandPrompt == -1 ? <>
+                                <div className='sectionClarify' style={{ marginTop: "20px" }}>
 
-                                        {sectionClarity?.map((element: any, idx: any) => (
-                                            (expandSectionClarity == idx || expandSectionClarity === -1) && expandPreQuestionClarity && <SectionClarify parentRef={cardRef} key={idx} title={element.pillName} questions={element.childPills} index={idx} onclick={(e) => setExpandSectionClarity(e)} element={currentSection} data={sectionData} from={"SECTION"} questionId={null} />
-                                        ))}
-                                    </div>
-                                    {expandPrompt == -1 &&
-                                        <Box className="buttonstyle1" style={{ marginBottom: '8px', paddingRight: '25px' }}>
-                                            <Button onClick={openPromptHandler} className="nextButton" endIcon={<EastRoundedIcon />}>Next</Button>
-                                        </Box>
-                                    }</> :
-                                    null
-                                }
-
-
-                                <div className='customeTyle'>
-                                    {currentSection['promptQuestionsMap'].map((prompt: any, promptIndex: number) => (
-                                        <Box key={promptIndex} className={`card123 ${promptIndex !== 0 ? 'mt-20' : ''}`}>
-                                            <div style={{background: "white"}} className='colorDetails'>
-                                                <div className='arrowbutton2'>
-                                                    <Button style={{ minWidth: "30px !important", width: "30px !important",background: "none", borderRadius: "25px", color: "#000000", height: "30px !important"}}><KeyboardArrowDownIcon /></Button>
-                                                </div>
-                                                <span className="detailsPrompt" style={{padding: "25px"}}>Prompt {promptIndex + 1} of {currentSection['promptQuestionsMap'].length}</span>
-                                                <div className='titleTextPrompt' style={{padding: "25px"}}>{prompt['question']}</div>
-                                            </div>
-
-                                            {expandPrompt == promptIndex &&
-
-                                                <>
-                                                    {answerAceepted ? <Button style={{ width: 'fit-content', background: '#ebf1f7', color: '#2e5db0', marginTop: '10px', }} onClick={() => setAnswerAceepted(false)}><Add /></Button> : <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px',}}  className='checkpadding'>
-                                                        {preQuestionClarity[promptIndex]?.questionPills.map((r: any, j: any) => (
-                                                            (expandPreQuestionClarity === -1 || expandPreQuestionClarity === j) && <SectionClarify childRef={childRef} setPromptSelect={setPromptSelect} parentRef={cardRef} key={j} title={r.pillName} questions={r.childPills} index={j} onclick={(e: any) => setExpandPreQuestionClarity(e)} element={currentSection} data={sectionData} from={"PRE"} questionId={preQuestionClarity[promptIndex].questionId} />
-                                                        ))
-                                                        }
-                                                    </div>}
-
-                                                    <PromptTextInput setAnswerAceepted={setAnswerAceepted} promptSelect={promptSelect} setPromptSelect={setPromptSelect} ref={childRef} />
-                                                    {
-                                                        answerAceepted && <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px', marginTop: '24px' }}>
-                                                            {postQuestionClarity[promptIndex]?.questionPills.map((r: any, j: any) => (
-                                                                (expandpostQuestionClarity === -1 || expandpostQuestionClarity === j) && <SectionClarify childRef={childRef} setPromptSelect={setPromptSelect} parentRef={cardRef} key={j} title={r.pillName} questions={r.childPills} index={j} onclick={(e: any) => setExpandpostQuestionClarity(e)} element={currentSection} data={sectionData} from={"POST"} questionId={postQuestionClarity[promptIndex].questionId} />
-                                                            ))
-                                                            }
-                                                        </div>
-                                                    }
-                                                    {
-                                                        answerAceepted && <Box className="buttonstyle" style={{ marginBottom: '8px', paddingRight: '25px' }}>
-                                                            <Button onClick={() => {
-                                                                resetAllState();
-
-                                                                if (currentSection['promptQuestionsMap'].length == promptIndex + 1) {
-                                                                    setExpandIndex(index + 1)
-                                                                } else {
-                                                                    setExpandPrompt(promptIndex + 1);
-                                                                }
-                                                            }}
-                                                                className="nextButton" endIcon={<EastRoundedIcon />}>Next</Button>
-                                                        </Box>
-                                                    }
-                                                </>
-
-                                            }
-                                        </Box>
+                                    {sectionClarity?.map((element: any, idx: any) => (
+                                        (expandSectionClarity == idx || expandSectionClarity === -1) && expandPreQuestionClarity && <SectionClarify parentRef={cardRef} key={idx} title={element.pillName} questions={element.childPills} index={idx} onclick={(e) => setExpandSectionClarity(e)} element={currentSection} data={sectionData} from={"SECTION"} questionId={null} />
                                     ))}
                                 </div>
+                                {expandPrompt == -1 &&
+                                    <Box className="buttonstyle1" style={{ marginBottom: '8px', paddingRight: '25px' }}>
+                                        <Button onClick={openPromptHandler} className="nextButton" endIcon={<EastRoundedIcon />}>Next</Button>
+                                    </Box>
+                                }</> :
+                                null
+                            }
+
+
+                            <div className='customeTyle'>
+                                {currentSection['promptQuestionsMap'].map((prompt: any, promptIndex: number) => (
+                                    <Box key={promptIndex} className={`card123 ${promptIndex !== 0 ? 'mt-20' : ''}`}>
+                                        <div style={{ background: "white" }} className='colorDetails'>
+                                            <div className='arrowbutton2'>
+                                                <Button style={{ minWidth: "30px !important", width: "30px !important", background: "none", borderRadius: "25px", color: "#000000", height: "30px !important" }}><KeyboardArrowDownIcon /></Button>
+                                            </div>
+                                            <span className="detailsPrompt" style={{ padding: "25px" }}>Prompt {promptIndex + 1} of {currentSection['promptQuestionsMap'].length}</span>
+                                            <div className='titleTextPrompt' style={{ padding: "25px" }}>{prompt['question']}</div>
+                                        </div>
+
+                                        {expandPrompt == promptIndex &&
+
+                                            <>
+                                                {answerAceepted ? <Button style={{ width: 'fit-content', background: '#ebf1f7', color: '#2e5db0', marginTop: '10px', }} onClick={() => setAnswerAceepted(false)}><Add /></Button> : <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
+                                                    {preQuestionClarity[promptIndex]?.questionPills.map((r: any, j: any) => (
+                                                        (expandPreQuestionClarity === -1 || expandPreQuestionClarity === j) && <SectionClarify childRef={childRef} setPromptSelect={setPromptSelect} parentRef={cardRef} key={j} title={r.pillName} questions={r.childPills} index={j} onclick={(e: any) => setExpandPreQuestionClarity(e)} element={currentSection} data={sectionData} from={"PRE"} questionId={preQuestionClarity[promptIndex].questionId} />
+                                                    ))
+                                                    }
+                                                </div>}
+
+                                                <PromptTextInput setAnswerAceepted={setAnswerAceepted} promptSelect={promptSelect} setPromptSelect={setPromptSelect} ref={childRef} />
+                                                {
+                                                    answerAceepted && <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px', marginTop: '24px' }}>
+                                                        {postQuestionClarity[promptIndex]?.questionPills.map((r: any, j: any) => (
+                                                            (expandpostQuestionClarity === -1 || expandpostQuestionClarity === j) && <SectionClarify childRef={childRef} setPromptSelect={setPromptSelect} parentRef={cardRef} key={j} title={r.pillName} questions={r.childPills} index={j} onclick={(e: any) => setExpandpostQuestionClarity(e)} element={currentSection} data={sectionData} from={"POST"} questionId={postQuestionClarity[promptIndex].questionId} />
+                                                        ))
+                                                        }
+                                                    </div>
+                                                }
+                                                {
+                                                    answerAceepted && <Box className="buttonstyle" style={{ marginBottom: '8px', paddingRight: '25px' }}>
+                                                        <Button onClick={() => {
+                                                            resetAllState();
+
+                                                            if (currentSection['promptQuestionsMap'].length == promptIndex + 1) {
+                                                                setExpandIndex(index + 1)
+                                                            } else {
+                                                                setExpandPrompt(promptIndex + 1);
+                                                            }
+                                                        }}
+                                                            className="nextButton" endIcon={<EastRoundedIcon />}>Next</Button>
+                                                    </Box>
+                                                }
+                                            </>
+
+                                        }
+                                    </Box>
+                                ))}
                             </div>
-                        </>
-                    }
-                </Paper>
-          
+                        </div>
+                    </>
+                }
+            </Paper>
+
 
         </>
     );
